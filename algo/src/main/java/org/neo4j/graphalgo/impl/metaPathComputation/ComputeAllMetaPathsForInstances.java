590bd6afb30c31d937eaaf7c830e5ca669a689e4
Michaell Vaichenker <Michael.Vaichenker@student.hpi.de>
2018-04-18 14:56:23 +0200

    changed Debugmessages

--- algo/src/main/java/org/neo4j/graphalgo/impl/metaPathComputation/ComputeAllMetaPathsForInstances.java
+++ algo/src/main/java/org/neo4j/graphalgo/impl/metaPathComputation/ComputeAllMetaPathsForInstances.java
@@ -131,15 +131,21 @@ public class ComputeAllMetaPathsForInstances extends MetaPathComputation {
 
     private boolean initializeNode(int node) {
         debugOut.println("INITILIZENODE");
+        if(arrayGraphInterface == null) debugOut.println("arrayGraphInterface");
         int nodeLabel = arrayGraphInterface.getLabel(node);
+        debugOut.println("nodeLabel: " + nodeLabel);
         int edgeLabel = arrayGraphInterface.getAllEdgeLabels().iterator().next();
+        debugOut.println("edgeLabel: " + edgeLabel);
+        if(labelDictionary == null) debugOut.println("labelDictionary");
+
         Integer nodeLabelId = labelDictionary.get(new AbstractMap.SimpleEntry<>(edgeLabel, nodeLabel));
+        if(initialInstances == null) debugOut.println("initialInstances");
         initialInstances.get(nodeLabelId).add(node);
         return true;
     }
 
     private int assignIdToNodeLabel(int edgeLabel, int nodeLabel) {
-        debugOut.println("ASSIGNIDTONODELABEL");
+        //debugOut.println("ASSIGNIDTONODELABEL");
         labelDictionary.put(new AbstractMap.SimpleEntry<>(edgeLabel, nodeLabel), currentLabelId);
         currentLabelId++;
         return currentLabelId - 1;
